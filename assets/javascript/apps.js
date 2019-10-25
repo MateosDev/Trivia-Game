d8e0eb9741d4bf43be753f83a36f7f41ce680a43
MateosDev <35548023+MateosDev@users.noreply.github.com>
2019-10-25 17:26:27 -0500

    updates js for basic game framework

--- assets/javascript/apps.js
+++ assets/javascript/apps.js
@@ -2,8 +2,48 @@ $("#start").on('click', function(){
     $("start").remove();
 });
 
+var game = {
+    questions:questions,
+    currentQuestion: 0,
+    timer: 90, 
+    correct: 0,
+    incorrect: 0,
+
+    countdown: function(){
+
+    },
+
+    loadQuestion: function(){
+
+    },
+
+    timeUp: function(){
+    
+    },
+
+    results: function(){
+
+    },
+
+    clicked: function(){
+
+    },
+
+    answeredCorrectly: function(){
+
+    },
+    answeredIncorrectly: function(){
+
+    },
+
+    reset: function(){
+
+    },
+}
+
 var questions = [{
     question: "Who is Mia married to?",
     answers: ["Marcellus Wallace", "Vincent Vega", "The Wolf" ],
     correctAnswer: "Marcellus Wallace",
-}]
+}];
+
