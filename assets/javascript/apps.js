a41e7532d092eb2506865ed93332b27ce3f85370
MateosDev <35548023+MateosDev@users.noreply.github.com>
2019-10-25 17:16:15 -0500

    updated js

---
+++ assets/javascript/apps.js
@@ -0,0 +1,9 @@
+$("#start").on('click', function(){
+    $("start").remove();
+});
+
+var questions = [{
+    question: "Who is Mia married to?",
+    answers: ["Marcellus Wallace", "Vincent Vega", "The Wolf" ],
+    correctAnswer: "Marcellus Wallace",
+}]
