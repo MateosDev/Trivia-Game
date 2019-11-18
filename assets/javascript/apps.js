a2d65d2817a7f0e05ed978a85634878edc9e1dd7
MateosDev <35548023+MateosDev@users.noreply.github.com>
2019-11-17 20:33:56 -0600

    updated index file and updated questions for game

--- assets/javascript/apps.js
+++ assets/javascript/apps.js
@@ -10,11 +10,17 @@ var game = {
     incorrect: 0,
 
     countdown: function(){
-
+        game.counter--;
+        $("#counter").html(game.counter);
+        if(game.counter<=0){
+            console.log("TIME UP");
+            game.timeUp();
+        }
     },
 
     loadQuestion: function(){
-
+        timer = setInterval(game.countdown, 9000);
+        $('subwrapper').html
     },
 
     timeUp: function(){
@@ -39,11 +45,33 @@ var game = {
     reset: function(){
 
     },
-}
+};
 
-var questions = [{
+var questions = [
+    
+    {
     question: "Who is Mia married to?",
     answers: ["Marcellus Wallace", "Vincent Vega", "The Wolf" ],
     correctAnswer: "Marcellus Wallace",
-}];
+    },
+
+    {
+    question: "What is Vincent Vega's Profession?",
+    answers: ["Undercover Cop" , "News Reporter" , "Hitman" ],
+    correctAnswer : "Hitman",
+    },
+
+    {
+    question: "What do you call a Quarter Pounder with Cheese in France?",    
+    answers: ["Le Big Mac" , "Royale with Cheese" , "Big Kahuna Burger" ],
+    correctAnswer : "Royale with Cheese"
+    },
+
+    {
+    question: "What are the names of the couple that try to rob the coffee shop?",
+    answers: ["Ringo and Yolanda" , "Will and Grace" , "Jody and Yvette"],
+    correctAnswer: "Ringo and Yolanda"
+    },
+
+];
 
