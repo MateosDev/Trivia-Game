ff8f7f71961b7191990424146af31b294a6181ee
MateosDev <35548023+MateosDev@users.noreply.github.com>
2019-11-18 03:14:52 -0600

    this was a hesvy commit. had issues with the css background - ochose to use a relavant image for the games theme. final functionality tested

--- assets/javascript/apps.js
+++ assets/javascript/apps.js
@@ -1,77 +1,171 @@
-$("#start").on('click', function(){
-    $("start").remove();
-});
-
-var game = {
-    questions:questions,
-    currentQuestion: 0,
-    timer: 90, 
+$(document).ready(function(){
+    $("#remaining-time").hide();
+    $("#start").on('click', trivia.startGame,);
+    $(document).on('click' , '.option', trivia.guessChecker);
+    
+    
+    
+  });
+  
+ 
+  var trivia = {
     correct: 0,
     incorrect: 0,
+    unanswered: 0,
+    currentSet: 0,
+    timer: 20,
+    timerOn: false,
+    timerId : '',
 
-    countdown: function(){
-        game.counter--;
-        $("#counter").html(game.counter);
-        if(game.counter<=0){
-            console.log("TIME UP");
-            game.timeUp();
-        }
+    questions: {
+        q1: "Who is Mia married to?",
+        q2: "What is Vincent Vega's Profession?",
+        q3: "What do you call a Quarter Pounder with Cheese in France?",    
+        q4: "What are the names of the couple that try to rob the coffee shop?",
+        q5: "What does Butch Coolidge do for a living?",
+        q6: "Who sold Vincent the heroin that Mia Wallace overdosed on?",
     },
+ 
+    
+       
 
-    loadQuestion: function(){
-        timer = setInterval(game.countdown, 9000);
-        $('subwrapper').html
-    },
-
-    timeUp: function(){
+    options: {
+      q1: ["Marcellus Wallace", "Vincent Vega" , "The Wolf" ],
+      q2: ["Undercover Cop" , "News Reporter" , "Hitman" ],
+      q3: ["Le Big Mac" , "Royale with Cheese" , "Big Kahuna Burger" ],
+      q4: ["Ringo and Yolanda" , "Will and Grace" , "Jody and Yvette" ],
+      q5: ["He's the butler" , "Boxer" , "Mr. Wolf's Assistant" ],
+      q6: ["The Gimp" , "Jules" , "Lance" ],
     
     },
 
-    results: function(){
-
+    answers: {
+      q1: "Marcellus Wallace" ,
+      q2: "Hitman",
+      q3: "Royale with Cheese",
+      q4: "Ringo and Yolanda",
+      q5: "Boxer",
+      q6: "Lance",
     },
-
-    clicked: function(){
-
-    },
-
-    answeredCorrectly: function(){
-
-    },
-    answeredIncorrectly: function(){
-
-    },
-
-    reset: function(){
-
-    },
-};
-
-var questions = [
     
-    {
-    question: "Who is Mia married to?",
-    answers: ["Marcellus Wallace", "Vincent Vega", "The Wolf" ],
-    correctAnswer: "Marcellus Wallace",
+
+    startGame: function(){
+      trivia.currentSet = 0;
+      trivia.correct = 0;
+      trivia.incorrect = 0;
+      trivia.unanswered = 0;
+      clearInterval(trivia.timerId);
+      
+      $('#game').show();
+      
+      $('#results').html('');
+      
+      $('#timer').text(trivia.timer);
+      
+      $('#start').hide();
+  
+      $('#remaining-time').show();
+      
+      trivia.nextQuestion();
+      
     },
 
-    {
-    question: "What is Vincent Vega's Profession?",
-    answers: ["Undercover Cop" , "News Reporter" , "Hitman" ],
-    correctAnswer : "Hitman",
+    nextQuestion : function(){
+      
+      trivia.timer = 10;
+       $('#timer').removeClass('last-seconds');
+      $('#timer').text(trivia.timer);
+      
+      if(!trivia.timerOn){
+        trivia.timerId = setInterval(trivia.timerRunning, 1000);
+      }
+      
+      var questionContent = Object.values(trivia.questions)[trivia.currentSet];
+      $('#question').text(questionContent);
+      
+      var questionOptions = Object.values(trivia.options)[trivia.currentSet];
+      
+      $.each(questionOptions, function(index, key){
+        $('#options').append($('<button class="option btn btn-info btn-lg">'+key+'</button>'));
+      })
+
+      
+    },
+    timerRunning : function(){
+
+      if(trivia.timer > -1 && trivia.currentSet < Object.keys(trivia.questions).length){
+        $('#timer').text(trivia.timer);
+        trivia.timer--;
+          if(trivia.timer === 4){
+            $('#timer').addClass('last-seconds');
+          }
+      }
+
+      else if(trivia.timer === -1){
+        trivia.unanswered++;
+        trivia.result = false;
+        clearInterval(trivia.timerId);
+        resultId = setTimeout(trivia.guessResult, 1000);
+        $('#results').html('<h3>Out of time! The answer was '+ Object.values(trivia.answers)[trivia.currentSet] +'</h3>');
+      }
+
+      else if(trivia.currentSet === Object.keys(trivia.questions).length){
+        
+        $('#results')
+          .html('<h3>Thank you for playing!</h3>'+
+          '<p>Correct: '+ trivia.correct +'</p>'+
+          '<p>Incorrect: '+ trivia.incorrect +'</p>'+
+          '<p>Unaswered: '+ trivia.unanswered +'</p>'+
+          '<p>Please play again!</p>');
+        
+        $('#game').hide();
+        
+        $('#start').show();
+
+        $('#boss').remove();
+      }
+      
     },
 
-    {
-    question: "What do you call a Quarter Pounder with Cheese in France?",    
-    answers: ["Le Big Mac" , "Royale with Cheese" , "Big Kahuna Burger" ],
-    correctAnswer : "Royale with Cheese"
-    },
+    guessChecker : function() {
+      
+      var resultId;
+      
+      var currentAnswer = Object.values(trivia.answers)[trivia.currentSet];
+      
+      
+      if($(this).text() === currentAnswer){
+        $(this).addClass('btn-success').removeClass('btn-info');
+        
+        trivia.correct++;
+        clearInterval(trivia.timerId);
+        resultId = setTimeout(trivia.guessResult, 1000);
+        $('#results').html('<h3>Correct Answer!</h3>');
+      }
 
-    {
-    question: "What are the names of the couple that try to rob the coffee shop?",
-    answers: ["Ringo and Yolanda" , "Will and Grace" , "Jody and Yvette"],
-    correctAnswer: "Ringo and Yolanda"
+      else{
+        $(this).addClass('btn-danger').removeClass('btn-info');     
+        trivia.incorrect++;
+        clearInterval(trivia.timerId);
+        resultId = setTimeout(trivia.guessResult, 1000);
+        $('#results').html('<h3>Mr. Wallace expects better! '+ currentAnswer +'</h3>');
+      }
+      
     },
+    guessResult : function(){
+      
+      trivia.currentSet++;
+      $('.option').remove();
+      $('#results h3').remove();
+      trivia.nextQuestion();
+       
+    }
+    
+  };
 
-];
+ 
+  
+  
+    console.log(trivia.answers);
+
 
